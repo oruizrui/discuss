b2e34625e9520197f0969e4bc2467e37fb2f706c
Oscar Ruiz <oscar.ruiz@kantox.com>
2020-11-29 16:59:27 +0100

    wip

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,17 +1,22 @@
 defmodule DiscussWeb.TopicController do
     use DiscussWeb, :controller
 
+    import Ecto.Query
+
     alias Discuss.Topic
-  
+
     def new(conn, _params) do
         changeset = Topic.changeset(%Topic{} , %{}) 
 
         render conn, "new.html", changeset: changeset
     end
 
-    def create(con, _params) do
+    def create(con, %{"topic" => topic}) do
+        changeset = Topic.changeset(%Topic{} , topic) 
 
-        
+        case Repo.insert(changeset) do
+            {:ok, post} -> IO.inspect(post)
+            {:error, changeset} -> IO.inspect(changeset)
+        end
     end
-  end
-  
+end
